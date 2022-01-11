fe25b7b0a894104f09e3740aef385bd07c40ad0c
Adrianas_Vitys <adrianalbujaira@gmail.com>
2022-01-11 12:22:04 +0100

    VIEW CREATION

--- Views/Creation.sql
+++ Views/Creation.sql
@@ -1,7 +1,20 @@
+-- VIEW CREATION -- 
+CREATE [OR REPLACE] [FORCE|NOFORCE] VIEW (NAME) AS (QUERY) [WITH CHECK OPTION [CONSTRAINT (CONSTRAINT NAME)]] [WITH READ ONLY]
+
+-- Remove a VIEW --
+DROP VIEW UNO;
+
+-- Select the VIEW --
+SELECT * FROM UNO;
+
+-------------------
+-- VIEW EXAMPLES --
+-------------------
+
 -- VIEW that show all employees with from department 10 and with a salary higuer than 1200 --
 CREATE VIEW UNO AS SELECT * FROM EMPLE WHERE DEPT_NO=10 AND SALARIO>1200;
 
--- VIEW 
+-- VIEW with each department, averrage salary and higuest salary --
 CREATE VIEW TRES (num_dep, nom_dep, media_salario, maximo_salario) AS 
 SELECT DEPART.dept_no, DEPART.dnombre, avg(salario), max(salario) FROM 
 DEPART, EMPLE WHERE DEPART.dept_no=EMPLE.dept_no GROUP BY DEPART.dept_no, DEPART.dnombre;
@@ -11,8 +24,3 @@ CREATE VIEW CUATRO (CENTRO, CONSERJE) AS SELECT NOMBRE, APELLIDOS
 FROM CENTROS, PERSONAL WHERE FUNCION = 'CONSERJE'
 AND CENTROS.COD_CENTRO = PERSONAL.COD_CENTRO;
 
--- Remove a VIEW --
-DROP VIEW UNO;
-
--- Select the VIEW --
-SELECT * FROM UNO;
